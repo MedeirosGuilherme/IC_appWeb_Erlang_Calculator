ea48a9aa8aa866fe9a120975c4694c699a222508
Guilherme Medeiros <guilherme.alth@gmail.com>
2020-11-24 23:01:30 -0300

    Capítulo 2: Templates e estruturas Jinja2

--- Curso Web-Python/microblog/app/routes.py
+++ Curso Web-Python/microblog/app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'Guilherme'},
+            'body': 'Lindo dia na Enseada!'
+        },
+        {
+            'author': {'username': 'Diego'},
+            'body': 'Comprei um fusca novo!'
+        },
+        {
+            'author': {'username': 'Alessandra'},
+            'body': 'Eu adorei o último filme do Ari Aster!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
